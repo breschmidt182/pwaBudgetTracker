ea796677a6b83fec4b23a85cb5170cf8ed373647
Bre <breanna.schmidt12@gmail.com>
2022-08-01 19:12:18 -0400

    finished up index and idb files

--- public/js/idb.js
+++ public/js/idb.js
@@ -37,8 +37,8 @@ let db;
   For the object store, something like "transactions".
 */
 
-const database = "your-variable-name-here"
-const objectStore = "your-variable-name-here"
+const database = "my-budget"
+const objectStore = "transactions"
 
 /*
   INFO: Here we tell indexedDb to open (or create) whatever database we 
@@ -68,7 +68,7 @@ request.onsuccess = ({ target }) => {
   db = target.result;
   // check if app is online before reading from db
   if (navigator.onLine) {
-    // method goes here
+    checkDatabase();
   }
 };
 
@@ -86,8 +86,8 @@ request.onerror = function(event) {
   variable.
 */
 function saveRecord(record) {
-  const transaction = db.transaction(["OBJECT_STORE"], "readwrite");
-  const store = transaction.objectStore("OBJECT_STORE");
+  const transaction = db.transaction([objectStore], "readwrite");
+  const store = transaction.objectStore(objectStore);
   store.add(record);
 }
 
@@ -104,8 +104,8 @@ function saveRecord(record) {
   variable.
 */
 function checkDatabase() {
-  const transaction = db.transaction(["OBJECT_STORE"], "readwrite");
-  const store = transaction.objectStore("OBJECT_STORE");
+  const transaction = db.transaction([objectStore], "readwrite");
+  const store = transaction.objectStore(objectStore);
   const getAll = store.getAll();
 
   getAll.onsuccess = function() {
@@ -120,7 +120,7 @@ function checkDatabase() {
         TODO: Insert the route name specified above.
       */
 
-      fetch("INSERT_UPDATE_ROUTE_NAME_HERE", {
+      fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
