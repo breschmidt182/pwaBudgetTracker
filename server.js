53d5d0e4b2494fdadba78f7e354aacffdfa7ca78
Bre <breanna.schmidt12@gmail.com>
2022-09-21 22:15:24 -0400

    changed server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
